be31473eb54e76edf118514f0a438f520214e686
Zuhaitz Beloki <z.beloki@elhuyar.eus>
2023-06-19 14:26:21 +0200

    Bash script added to generate & train in a single script

---
+++ generate_and_train.sh
@@ -0,0 +1,7 @@
+# NAME
+# SENTS
+# SEED
+
+python3 generate_synthetic_data.py $SENTS $CORRUPTION/wi/type_classifier $CORRUPTION/wi/span_classifier $CORRUPTION/wi/esg_model $TRAIN_WI_M2 $SYNTH/$NAME.csv $SEED;
+
+DEV=$DEV_WI_M2 bash train.sh $SYNTH/$NAME.csv $CORRECTION/$NAME
