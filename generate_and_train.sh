b75a5b473ac24977ef9adf49c5296870d108d150
Zuhaitz Beloki <z.beloki@elhuyar.eus>
2023-06-19 14:27:27 +0200

    Fixed seed

--- generate_and_train.sh
+++ generate_and_train.sh
@@ -1,6 +1,6 @@
 # NAME
 # SENTS
-# SEED
+SEED=59
 
 python3 generate_synthetic_data.py $SENTS $CORRUPTION/wi/type_classifier $CORRUPTION/wi/span_classifier $CORRUPTION/wi/esg_model $TRAIN_WI_M2 $SYNTH/$NAME.csv $SEED;
 
